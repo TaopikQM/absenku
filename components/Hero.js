8cdd2e5788afe0bf1064023073553dfd04ef1a0c
TaopikQM <taopik61318@gmail.com>
2024-09-03 15:18:06 +0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -42,9 +42,9 @@ const Hero = ({
                 ABSEN YUK! <strong>Mulai dari kamu</strong>.
               </h1>
              
-                <div className={"py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none " }>
+                <button className={"py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none " }>
                   <Faces/>
-                </div>
+                </button>
 
                 
 
